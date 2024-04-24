4095e6f14536d2af1456dddc1acef50582732a86
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-04-24 01:38:41 -0700

    Initial commit

---
+++ lua/tomo/init.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+--- Put the `char` variable at the beginning of the line. If present, remove it.
+--- Thanks to https://github.com/mrs4ndman/random.nvim
+---@param chars string
+function M.put_at_end(chars)
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local row = pos[1] - 1
+    local current_line = vim.api.nvim_get_current_line()
+    local col = #current_line
+    local entry_length = string.len(chars)
+    local cline = vim.fn.getline(".")
+    local endchar = vim.fn.getline("."):sub(cline:len() - (entry_length - 1))
+    if endchar == chars then
+        vim.api.nvim_set_current_line(cline:sub(1, cline:len() - entry_length))
+    else
+        vim.api.nvim_buf_set_text(0, row, col, row, col, { chars })
+    end
+end
+
+return M
